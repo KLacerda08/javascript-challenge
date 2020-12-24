2efc313a469e9a024e25b856d5fce7eb4b6e7413
KLacerda08 <kaldmr7880@gmail.com>
2020-12-24 11:07:28 -0500

    completed working code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // assign data from data.js to a descriptive variable
 var ufoRecords = data;
 
@@ -51,8 +33,6 @@ form.on("submit", runEnter);
 
 // Complete the event handler function
 function runEnter() {
-    //clear the table 
-    tbody.html("");   
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
     // Get the value property of the input element
@@ -61,56 +41,12 @@ function runEnter() {
     d3.event.preventDefault();
     // create a filtered data set 
     var filteredData = ufoRecords.filter(record => record.datetime === inputValue);
-    // // Use d3 to add rows for each filtered UFO sighting record, add cells, populate each cell
-    // filteredData.forEach(function(record) {
-    //     // console.log(record);
-    //     var tbody = d3.select("#ufo-table").select("tbody");
-    //     var row = tbody.append("tr");
-    //     Object.entries(record).forEach(function([key, value]) {
-    //     // console.log(key, value);
-    //         var cell = row.append("td");
-    //         cell.text(value);
-    //     });
-    // });
-    // build new table with filtered data
     console.log(filteredData);
-    buildTable(filteredData);
+    //clear the table 
+    tbody.html("");
+    // build the filteredtable if the input value is not blank 
+    if (inputValue !== "") {
+        buildTable(filteredData);}
+        else {
+            buildTable(ufoRecords);}
 };
-
-// // call the function
-// runEnter();
-// }
-
-
-
-
-        // var data = filteredData(function(record.datetime) {
-    // //     return (
-    //     table.append("tr").text(`${record.datetime}`),
-        // table.append("tr").text(`${record.state}`),
-        // table.append("tr").text(`${record.country}`),
-        // table.append("tr").text(`${record.shape}`),
-        // table.append("tr").text(`${record.durationMinutes}`),
-        // table.append("tr").text(`${record.comments}`),
- 
-
-
-    // var patientsAsString = patients.join(', ');
-
-
-
-    // // remove any children from the table
-    // table.html("");
-
-    // define variables?
-    // var patients = filteredData.map(function(patient) {
-    //     return (`${patient.fullName}: ${patient.age}`);
-    //   });
-    
-    // var date = filteredData.map(function(record) {
-    //     return (`${record.datetime}`);
-    //   });
- 
-
-    //example of selecting nested elements
-    // d3.select("tr").selectAll("td").style('background-color','yellow');
